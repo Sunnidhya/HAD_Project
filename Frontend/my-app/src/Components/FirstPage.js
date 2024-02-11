92d768faf5f846247d5c2ead65b63afd6714b66d
Sunnidhya Roy <sunnidhya.roy@gmail.com>
2024-02-11 18:29:10 +0530

    Updated the Doctor Login and Radiologist Login Pages

---
+++ Frontend/my-app/src/Components/FirstPage.js
@@ -0,0 +1,14 @@
+import './FirstPage.css';
+import DoctorLogin from './Doctor/Login/DoctorLogin';
+import RadioLogin from './Radiologist/Login/RadioLogin';
+import imgside from '../Resources/AppLogo.png';
+
+function FirstPage() {
+  return (
+    <>
+    <RadioLogin/>
+    </>
+  );
+}
+
+export default FirstPage;
