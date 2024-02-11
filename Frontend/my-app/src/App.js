897c2e09a42117bc35bb41e924c0f0d61343781f
Sunnidhya Roy <sunnidhya.roy@gmail.com>
2024-02-11 15:20:54 +0530

    Updated the common login page to be used for every module with color and image changes

--- Frontend/my-app/src/App.js
+++ Frontend/my-app/src/App.js
@@ -1,15 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-import RadioLogin from './Components/Radiologist/RadioLogin';
-import { Link,Route,Routes,Router } from 'react-router-dom';
+import RadioLogin from './Components/Radiologist/Login/RadioLogin';
+import imgside from './Resources/AppLogo.png';
 
 function App() {
   return (
-    <RadioLogin></RadioLogin>
-    
-
-
-
+    <>
+    <div class="Radio-Login-hor">
+        <div>
+          <img src={imgside} id="radseideimg" />
+        </div>
+        <div className='divisions1'>
+          <h1 className="pageTitle">Kavach - India's Leading Tele-Radiology Platform</h1>
+        </div>
+    </div>
+    <RadioLogin/>
+    </>
   );
 }
 
