6db74ce97fa21b31445ba874748196ebfb07d66a
codeboyshamik99 <121502383+codeboyshamik99@users.noreply.github.com>
2024-03-01 22:17:50 +0530

    Updated Profile Page

--- Frontend/my-app/src/App.js
+++ Frontend/my-app/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import FirstPage from './Components/FirstPage';
+import Profile from './Components/Radiologist/Profile/Profile';
 
 function App() {
   return (
     <>
-      <FirstPage/>
+      <Profile/>
     </>
   );
 }
