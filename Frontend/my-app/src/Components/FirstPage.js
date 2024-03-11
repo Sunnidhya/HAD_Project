0bd1adee9a7ec115ef77158a0e9ec4e9a2dea957
Samarpita Bhaumik <samarpitabhaumik2017@gmail.com>
2024-03-11 19:06:02 +0530

    update the first page

--- Frontend/my-app/src/Components/FirstPage.js
+++ Frontend/my-app/src/Components/FirstPage.js
@@ -17,46 +17,43 @@ function FirstPage() {
       <div class="icon-button">
         <i class="fas fa-user"></i>
         Admin
-        {/* <i class="fas fa-chevron-right"></i> */}
+        <i class="fas fa-chevron-right"/>
       </div>
       <div class="icon-button">
-        <i class="fas fa-user"></i>
+        <i class="fas fa-stethoscope"></i>
         Doctor
-        {/* <i class="fas fa-chevron-right"></i> */}
+        <i class="fas fa-chevron-right"></i>
       </div>
       <div class="icon-button">
-        <i class="fas fa-user"></i>
+        <i class="fas fa-user-md"></i>
         Radiologist
-        {/* <i class="fas fa-chevron-right"></i> */}
+        <i class="fas fa-chevron-right"></i>
       </div>
       <div class="icon-button">
-        <i class="fas fa-user"></i>
+        <i class="fas fa-x-ray"></i>
         Lab
-        {/* <i class="fas fa-chevron-right"></i> */}
+        <i class="fas fa-chevron-right"></i>
       </div>
       <div class="icon-button">
-        <i class="fas fa-user"></i>
+        <i class="fas fa-hospital-user"></i>
         Patient
-        {/* <i class="fas fa-chevron-right"></i> */}
+        <i class="fas fa-chevron-right"></i>
+      </div>
+      <div class="left-bottom">
+      © 2024, Kavach Inc.
       </div>
     </div>
     
     <div class="right">
+      <div class="bottom"></div>
+      <div class="bg-text">
+      <h1> Welcome to Kavach</h1>
+      <blockquote>
+       <p>Kavach stands as a pioneering force in the realm of teleradiology, providing a leading-edge platform for medical imaging interpretation and analysis.<br/>With its advanced technology and expert radiologists,Kavach ensures rapid and accurate diagnosis, enabling healthcare providers to deliver timely and precise patient care.<br/>Its seamless integration with healthcare systems allows for effortless transmission and interpretation of medical images, empowering healthcare professionals with actionable insights from anywhere in the world.<br/>Kavach's commitment to excellence and innovation makes it a trusted partner in the journey towards improved healthcare outcomes,setting a benchmark in the field of telemedicine and radiology services.<br/></p>
+       </blockquote>
       
-      <div class="top">
-        <header>
-        <h1>Welcome <span class="blue">to</span> Kavach</h1>
-        </header>
+
       </div>
-      <div class="bottom">
-        <div class="imageleft">
-          <img id='WelcomeImage'src={firstImage} alt="Image Left"/>
-        </div>
-        <div class="imageright">
-          <img id='Image'src={secondImage} alt="Image Right"/>
-        </div>
-      </div>
-     
     </div>
   </div>
   );
