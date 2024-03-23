e3a6ae7e1bcf2127daed535280c8b4963b86a19e
codeboyshamik99 <121502383+codeboyshamik99@users.noreply.github.com>
2024-03-23 15:13:01 +0530

    Made changes in AdminHomePage

--- Frontend/my-app/src/App.js
+++ Frontend/my-app/src/App.js
@@ -8,21 +8,25 @@ import PatientLogin from './Components/Patient/Login/PatientLogin';
 import PatientLanding from './Components/Patient/PatientLanding/PatientLanding';
 import RegPatient from './Components/Patient/Registration/RegPatient';
 import RadioLogin from './Components/Radiologist/Login/RadioLogin';
+import AdminHomePage from './Components/Admin/landing/AdminHomePage';
+import Profile from './Components/Radiologist/Profile/Profile';
 import { Routes, Route } from 'react-router-dom';
 
 
 function App() {
   return (
-      <Routes>
-        <Route path="/" element={<FirstPage/>}/>
-        <Route path="/admin" element={<AdminLogin/>}/>
-        <Route path="/doctor" element={<DoctorLogin/>}/>
-        <Route path="/radiologist" element={<RadioLogin/>}/>
-        <Route path="/lab" element={<LabLogin/>}/>
-        <Route path="/patient" element={<PatientLogin/>}/>
-        <Route path="/patient/landing" element={<PatientLanding/>}/>
-        <Route path="/doctor/landing" element={<DoctorLanding/>}/>
-      </Routes>
+    <Routes>
+    <Route path="/" element={<Profile/>}/>
+    <Route path="/admin" element={<AdminLogin/>}/>
+    <Route path="/doctor" element={<DoctorLogin/>}/>
+    <Route path="/radiologist" element={<RadioLogin/>}/>
+    <Route path="/lab" element={<LabLogin/>}/>
+    <Route path="/patient" element={<PatientLogin/>}/>
+    <Route path="/patient/landing" element={<PatientLanding/>}/>
+    <Route path="/doctor/landing" element={<DoctorLanding/>}/>
+  </Routes>
+
+
   );
 }
 
---
+++ Frontend/my-app/src/Components/Admin/landing/AdminHomePage.js
@@ -0,0 +1,61 @@
+import imgmain from '../../../Resources/login-hero.svg';
+import userIcon from '../../../Resources/UserIcon.png';
+import passwordIcon from '../../../Resources/PasswordIcon.png';
+import imgside from '../../../Resources/AppLogo.png';
+import profile from '../../../Resources/Profile.png';
+import user from '../../../Resources/IconProfile.png';
+import star1 from '../../../Resources/star.jpg';
+import admin from '../../../Resources/Picture1.png';
+import React, { useState } from 'react';
+import './AdminHomePage.css'
+const Profile = () => {
+  const [patientValue, setPatientValue] = useState(12);
+  const [radiologistValue, setRadiologistValue] = useState(22);
+  const [doctorValue, setDoctorValue] = useState(22);
+  const [labValue, setLabValue] = useState(12);
+    return (
+      <div class="Admin-Login-container">
+        <div class="Admin-Login-hor">
+          <div>
+            <img src={imgside} id="radseideimg" />
+          </div>
+          <div className='divisions1'>
+             <h1 className="pageTitle">Kavach - India's Leading Tele-Radiology Platform</h1>
+          </div>
+        </div>
+
+        <div className="vertical-box">
+           <img src={admin} alt="Admin Icon" className="user-icon" />
+        </div>
+
+        <div className="vertical-box1">
+          <div className='box1'>
+              <h2>Patient</h2>
+              <p>{patientValue}</p>
+          </div>
+          <br/>
+          <div className='box2'>
+            <h2>Radiologist</h2>
+            <p>{radiologistValue}</p>
+          </div>
+        </div>
+
+        <div className="vertical-box2">
+          <div className='box3'>
+          <h2>Doctor</h2>
+          <p>{doctorValue}</p>
+          </div>
+          <br/>
+          <div className='box4'>
+          <h2>Lab</h2>
+          <p>{labValue}</p>
+          </div>
+        </div>
+
+        <div className="footer1">
+          <footer>About Us</footer>
+        </div>
+    </div>
+    );
+}
+export default Profile;
