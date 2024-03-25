20dc24556be9948f7a75ada86d98f5838ff7b092
subhankhi1002 <subhankhimaiti@gmail.com>
2024-03-25 22:48:00 +0530

    Updated Admin Landing Basic

--- Frontend/my-app/src/App.js
+++ Frontend/my-app/src/App.js
@@ -8,11 +8,13 @@ import PatientLogin from './Components/Patient/Login/PatientLogin';
 import PatientLanding from './Components/Patient/PatientLanding/PatientLanding';
 import RegPatient from './Components/Patient/Registration/RegPatient';
 import RadioLogin from './Components/Radiologist/Login/RadioLogin';
-import AdminHomePage from './Components/Admin/landing/AdminHomePage';
+
 import Profile from './Components/Radiologist/Profile/Profile';
 import { Routes, Route } from 'react-router-dom';
 import LabLanding from './Components/Lab/Landing/LabLanding';
 import RadioLanding from './Components/Radiologist/Landing/RadiologistLanding';
+import AdminLanding from './Components/Admin/landing/AdminLanding';
+
 
 
 function App() {
@@ -29,7 +31,7 @@ function App() {
     <Route path="/doctor/landing" element={<DoctorLanding/>}/>
     <Route path="/lab/landing" element={<LabLanding/>}/>
     <Route path="/radiologist/landing" element={<RadioLanding/>}/>
-    <Route path="/admin/landing" element={<AdminHomePage/>}/>
+    <Route path="/admin/landing" element={<AdminLanding/>}/>
   </Routes>
 
 
