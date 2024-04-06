33b59ea39476143a228ea8b8b13f1dcf43089ff7
codeboyshamik99 <121502383+codeboyshamik99@users.noreply.github.com>
2024-04-06 15:02:06 +0530

    Retrieved list of Patients

--- Frontend/my-app/src/Components/Form/CaseForm.js
+++ Frontend/my-app/src/Components/Form/CaseForm.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './CaseForm.css';
 import './DropdownButton.css';
-import { createcase } from '../../Network/APIendpoints';
+import { createcase, getListOfPatients } from '../../Network/APIendpoints';
 import { request } from '../../Network/axiosHelper';
 import DropdownButton from './Dropdown_button';
 
@@ -11,7 +11,23 @@ function CaseForm() {
     doctorName: '',
     patientName: ''
   });
+  const [patients, setPatients] = useState([]);
   const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    fetchPatients();
+  }, []);
+
+  const fetchPatients = () => {
+    request("GET", getListOfPatients)
+      .then((response) => {
+        setPatients(response.data); 
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.warn("Error fetching patients", error);
+      });
+  };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -23,33 +39,33 @@ function CaseForm() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    request("POST",createcase , formData)
-    .then((response) => {
-     alert(response.data.message)
-    })
-    .catch((error) => {
-      console.warn("Error", error);
-    });
+    request("POST", createcase, formData)
+      .then((response) => {
+        alert(response.data.message);
+      })
+      .catch((error) => {
+        console.warn("Error", error);
+      });
   };
 
   const toggleVisibility = () => {
-    setIsVisible(prevIsVisible => !prevIsVisible);
+    setIsVisible((prevIsVisible) => !prevIsVisible);
   };
 
   const stopPropagation = (e) => {
     e.stopPropagation();
   };
 
-  
-
   return (
     <>
       {isVisible && (
         <div className="form-container-case" onClick={stopPropagation}>
-          <button className="close-button-case" onClick={toggleVisibility}>X</button>
+          <button className="close-button-case" onClick={toggleVisibility}>
+            X
+          </button>
           <div className="container-case">
             <h2 className="form-header">Case Creation</h2>
-            <form className="case-form" onSubmit={handleSubmit} >
+            <form className="case-form" onSubmit={handleSubmit}>
               <label>
                 Case Name:
                 <input
@@ -57,7 +73,7 @@ function CaseForm() {
                   name="caseName"
                   placeholder="Enter Case Name"
                   value={formData.caseName}
-                  style={{border:"1.3px solid"}}
+                  style={{ border: "1.3px solid" }}
                   onChange={handleChange}
                 />
               </label>
@@ -69,20 +85,23 @@ function CaseForm() {
                   name="doctorName"
                   placeholder="Enter doctor user name"
                   value={formData.doctorName}
-                  style={{border:"1.3px solid"}}
+                  style={{ border: "1.3px solid" }}
                   onChange={handleChange}
                 />
               </label>
               <br />
               <label>
                 Patient Name:
-                <DropdownButton className="fixed-button"
-                  onSelect={(selectedOption) => setFormData({...formData, patientName: selectedOption})}
+                <DropdownButton
+                  onSelect={(selectedOption) =>
+                    setFormData({ ...formData, patientName: selectedOption })
+                  }
                   selectedOption={formData.patientName}
+                  options={patients.map((patient) => patient.userName)}
                 />
               </label>
               <br />
-              <button type="submit" >Submit</button>
+              <button type="submit">Submit</button>
             </form>
           </div>
         </div>
--- Frontend/my-app/src/Components/Form/Dropdown_button.js
+++ Frontend/my-app/src/Components/Form/Dropdown_button.js
@@ -1,25 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect} from 'react';
+import { request } from '../../Network/axiosHelper';
 import Dropdown from 'react-bootstrap/Dropdown';
+import { createcase, getListOfPatients } from '../../Network/APIendpoints';
 import './DropdownButton.css'; 
 
 function DropdownButton() {
+  const [formData, setFormData] = useState({
+    caseName: '',
+    doctorName: '',
+    patientName: ''
+  });
   const [selectedOption, setSelectedOption] = useState(null);
-  const options = ['Doctor', 'Patient', 'Radiologist'];
+  const [patients, setPatients] = useState([]);
 
   const handleOptionClick = (option) => {
     setSelectedOption(option);
   };
 
+  useEffect(() => {
+    fetchPatients();
+  }, []);
+
+  const fetchPatients = () => {
+    request("GET", getListOfPatients)
+      .then((response) => {
+        setPatients(response.data); 
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.warn("Error fetching patients", error);
+      });
+  };
   return (
     <div className="center-container">
-      <Dropdown>
+      <Dropdown style={{ width: '100%' }}>
         <Dropdown.Toggle variant="success" id="dropdown-basic" className="fixed-button">
-          {selectedOption ? selectedOption : 'Dropdown Button'}
+          {selectedOption ? selectedOption : 'Select Patient Name'}
         </Dropdown.Toggle>
 
-        <Dropdown.Menu>
-          {options.map(option => (
-            <Dropdown.Item key={option} className="fixed-button" onClick={() => handleOptionClick(option)}>{option}</Dropdown.Item>
+        <Dropdown.Menu style={{ width: '100%' }}>
+        {patients.map((patient) => (
+            <Dropdown.Item key={patient.id} onClick={() => handleOptionClick(patient.userName)}>
+              {patient.userName}
+            </Dropdown.Item>
           ))}
         </Dropdown.Menu>
       </Dropdown>
